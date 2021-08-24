a1f56c908bbc09696837285aeb76708fefc381a9
Fabio Muratori <fabio.muratori2@studio.unibo.it>
2021-08-24 16:06:34 +0200

    minor refactoring of enemy entity state

--- src/main/scala/model/entities/Enemies.scala
+++ src/main/scala/model/entities/Enemies.scala
@@ -14,21 +14,18 @@ trait Score {
 class EnemyImpl(private var entityBody: EntityBody, private val size: (Float, Float), private val score: Int = 100)
                   extends LivingEntityImpl(entityBody, size) with Enemy with Score {
 
+  val attackDamage = 10
+
   override def getType(): Unit = ???
 
   override def getScore: Int = this.score
-
-  val attackDamage = 10
 
   override def update(): Unit = {
     this.move // movementStrategy.move()
     attackStrategy.apply()
 
-//    if (!attackStrategy.isAttackFinished) println("attacking")
-//
-//    else if (this.entityBody.getBody.getLinearVelocity.x != 0) println("moving")
-//
-//    else println("idle")
-
+    if (!attackStrategy.isAttackFinished) this.state = State.Attack01
+    else if (this.entityBody.getBody.getLinearVelocity.x != 0) this.state = State.Running
+    else this.state = State.Standing
   }
 }
