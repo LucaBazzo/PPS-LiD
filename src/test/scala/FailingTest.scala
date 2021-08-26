d09f96a065f724d16d59fae2b4728ce8947773f4
Luca Bracchi <luca.bracchi3@studio.unibo.it>
2021-08-26 10:44:54 +0200

    ignore FailingTest

--- src/test/scala/FailingTest.scala
+++ src/test/scala/FailingTest.scala
@@ -1,10 +1,11 @@
 package scala
 
-import org.junit.Test
+import org.junit.{Ignore, Test}
 import org.junit.Assert.assertEquals
 
 class FailingTest {
 
+  @Ignore
   @Test
   def failingTest(): Unit = {
     assertEquals(1,2)
