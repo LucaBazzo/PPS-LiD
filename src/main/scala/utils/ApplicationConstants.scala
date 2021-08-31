59708ca8dab5f1fa181c0021232327232fe881f2
Luca Bracchi <luca.bracchi3@studio.unibo.it>
2021-08-31 16:36:39 +0200

    decreased hero vertical size while sliding

--- src/main/scala/utils/ApplicationConstants.scala
+++ src/main/scala/utils/ApplicationConstants.scala
@@ -21,5 +21,5 @@ object ApplicationConstants {
 
   val HERO_SIZE: (Float, Float) = (8.5f, 15f)
 
-  val HERO_SIZE_SMALL: (Float, Float) = (8.5f, 8f)
+  val HERO_SIZE_SMALL: (Float, Float) = (8.5f, 6f)
 }
