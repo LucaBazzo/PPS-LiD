df20396a32bb79a3828ce01fee0bdb09ca677f9e
Fabio Muratori <fabio.muratori2@studio.unibo.it>
2021-09-06 11:52:16 +0200

    added first static room generation for testing

--- src/main/scala/controller/ControllerImpl.scala
+++ src/main/scala/controller/ControllerImpl.scala
@@ -1,12 +1,13 @@
 package controller
 
-import _root_.utils.ApplicationConstants.GAME_LOOP_STEP
+import _root_.utils.ApplicationConstants.{GAME_LOOP_STEP, ROOM_MAP_NAMES}
 import controller.GameEvent.GameEvent
 import model._
 import model.helpers.EntitiesContainerMonitor
 import view._
 
 import java.util.concurrent.{Executors, ScheduledExecutorService, TimeUnit}
+import scala.util.Random
 
 /** Handles almost every aspect that allows the game to run such as starting and
  * stopping the game loop, initializing view and model, saving existing game run
@@ -26,13 +27,9 @@ class ControllerImpl extends Controller with Observer {
   private val observerManager: ObserverManager = new ObserverManagerImpl()
   this.observerManager.addObserver(this)
 
-  // TODO: riabilitare la generazione casuale della mappa
-  private var rooms: Array[String] = Array("room1-final")
-  /*
-    private var rooms: Array[String] = Array("hero-room")
-    for(n <- 1 to 2) rooms = rooms :+ ROOM_MAP_NAMES(Random.between(0,ROOM_MAP_NAMES.size))
-  */
-  // rooms = rooms :+ "boss-room"
+  private var rooms: Array[String] = Array("hero-room", "room1-final")
+  for(n <- 1 to 2) rooms = rooms :+ ROOM_MAP_NAMES(Random.between(0,ROOM_MAP_NAMES.size))
+//  rooms = rooms :+ "boss-room"
 
   private val view: View = new ViewImpl(entitiesContainer, observerManager, rooms)
   private val model: Model = new ModelImpl(entitiesContainer, rooms)
