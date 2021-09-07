e62b071437ef7d5c751b5efc90ade289910a21e3
Luca Bracchi <luca.bracchi3@studio.unibo.it>
2021-09-07 18:44:50 +0200

    cleaned code

--- src/main/scala/view/screens/helpers/TileMapHelper.scala
+++ src/main/scala/view/screens/helpers/TileMapHelper.scala
@@ -1,12 +1,10 @@
 package view.screens.helpers
 
-import _root_.utils.ApplicationConstants.{PIXELS_PER_METER}
-import com.badlogic.gdx.maps.MapProperties
+import _root_.utils.ApplicationConstants.PIXELS_PER_METER
 import com.badlogic.gdx.maps.objects.RectangleMapObject
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer
 import com.badlogic.gdx.maps.tiled.{TiledMap, TmxMapLoader}
 import com.badlogic.gdx.math.Rectangle
-import model.Level
 import model.collisions.EntityCollisionBit
 import model.collisions.ImplicitConversions._
 import model.entities.EntityType
@@ -25,7 +23,7 @@ object TileMapHelper {
   def getTiledMap(mapName: String, offset: (Integer, Integer)): TiledMap = {
     val tiledMap: TiledMap = new TmxMapLoader().load("assets/maps/" + mapName + ".tmx")
 
-    tiledMap.getLayers().forEach(layer => {
+    tiledMap.getLayers.forEach(layer => {
       layer.setOffsetX(offset._1*8)
       layer.setOffsetY(offset._2*8)
     })
@@ -41,40 +39,24 @@ object TileMapHelper {
     var rect: Rectangle = new Rectangle()
     val tiledMap: TiledMap = new TmxMapLoader().load(path)
 
-    tiledMap.getLayers().forEach(layer => {
-      layer.getObjects().forEach(obj => {
+    tiledMap.getLayers.forEach(layer => {
+      layer.getObjects.forEach(obj => {
         rect = obj.asInstanceOf[RectangleMapObject].getRectangle
 
         val size: (Float, Float) = (rect.getWidth, rect.getHeight)
         val position: (Float, Float) = (
-          (rect.getX*2 + rect.getWidth + offset._1*16),
-          (rect.getY*2 + rect.getHeight - offset._2*16))
+          rect.getX*2 + rect.getWidth + offset._1*16,
+          rect.getY*2 + rect.getHeight - offset._2*16)
 
-        layer.getName() match {
-          case "ground" | "bridge" => {
-            spawnEntity(() => EntitiesFactoryImpl.createImmobileEntity(EntityType.Immobile, size, position, EntityCollisionBit.Immobile, EntityCollisionBit.Hero | EntityCollisionBit.Enemy | EntityCollisionBit.Arrow | EntityCollisionBit.EnemyAttack))
-          }
-          case "door" => {
-            spawnEntity(() => EntitiesFactoryImpl.createDoor(size, position))
-          }
-          case "chest" => {
-            spawnEntity(() => EntitiesFactoryImpl.createImmobileEntity(EntityType.Immobile, size, position, EntityCollisionBit.Immobile, EntityCollisionBit.Hero | EntityCollisionBit.Enemy | EntityCollisionBit.Arrow | EntityCollisionBit.EnemyAttack))
-          }
-          case "ladder" => {
-            spawnEntity(() => EntitiesFactoryImpl.createLadder(position, size))
-          }
-          case "water" => {
-            spawnEntity(() => EntitiesFactoryImpl.createWaterPool(position,size))
-          }
-          case "lava" => {
-            spawnEntity(() => EntitiesFactoryImpl.createLavaPool(position, size))
-          }
-          case "enemy" => {
-            spawnEntity(() => EntitiesFactoryImpl.createEnemies(size, position))
-          }
-          case _ => {
-            println("not supported layer: " + layer.getName())
-          }
+        layer.getName match {
+          case "ground" | "bridge" => spawnEntity(() => EntitiesFactoryImpl.createImmobileEntity(EntityType.Immobile, size, position, EntityCollisionBit.Immobile, EntityCollisionBit.Hero | EntityCollisionBit.Enemy | EntityCollisionBit.Arrow | EntityCollisionBit.EnemyAttack))
+          case "door" => spawnEntity(() => EntitiesFactoryImpl.createDoor(size, position))
+          case "chest" => spawnEntity(() => EntitiesFactoryImpl.createImmobileEntity(EntityType.Immobile, size, position, EntityCollisionBit.Immobile, EntityCollisionBit.Hero | EntityCollisionBit.Enemy | EntityCollisionBit.Arrow | EntityCollisionBit.EnemyAttack))
+          case "ladder" => spawnEntity(() => EntitiesFactoryImpl.createLadder(position, size))
+          case "water" => spawnEntity(() => EntitiesFactoryImpl.createWaterPool(position,size))
+          case "lava" => spawnEntity(() => EntitiesFactoryImpl.createLavaPool(position, size))
+          case "enemy" => spawnEntity(() => EntitiesFactoryImpl.createEnemies(size, position))
+          case _ => println("not supported layer: " + layer.getName)
         }
       })
 
