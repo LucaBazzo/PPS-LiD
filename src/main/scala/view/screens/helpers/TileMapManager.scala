033d3bdafa0db6c51cb5911991c1ca98bbd9f908
Luca Bracchi <luca.bracchi3@studio.unibo.it>
2021-09-12 10:49:03 +0200

    refactored TileMapHelper

--- src/main/scala/view/screens/helpers/TileMapManager.scala
+++ src/main/scala/view/screens/helpers/TileMapManager.scala
@@ -34,20 +34,20 @@ class TileMapHelper {
     }
 
     this.tiledMapList = Array(
-      (getTiledMap(WORLD_LEFT_BORDER_NAME), WORLD_LEFT_BORDER_NAME, WORLD_LEFT_BORDER_OFFSET),
-      (getTiledMap(WORLD_TOP_BORDER_NAME), WORLD_TOP_BORDER_NAME, WORLD_TOP_BORDER_OFFSET),
-      (getTiledMap(WORLD_RIGHT_BORDER_NAME), WORLD_RIGHT_BORDER_NAME, WORLD_RIGHT_BORDER_OFFSET),
-      (getTiledMap(WORLD_BOTTOM_BORDER_NAME), WORLD_BOTTOM_BORDER_NAME, WORLD_BOTTOM_BORDER_OFFSET),
-      (getTiledMap(HERO_ROOM_MAP_NAME), HERO_ROOM_MAP_NAME, HERO_ROOM_OFFSET),
+      (getTiledMap(WORLD_LEFT_BORDER_NAME), null, WORLD_LEFT_BORDER_OFFSET),
+      (getTiledMap(WORLD_TOP_BORDER_NAME), null, WORLD_TOP_BORDER_OFFSET),
+      (getTiledMap(WORLD_RIGHT_BORDER_NAME), null, WORLD_RIGHT_BORDER_OFFSET),
+      (getTiledMap(WORLD_BOTTOM_BORDER_NAME), null, WORLD_BOTTOM_BORDER_OFFSET),
+      (getTiledMap(HERO_ROOM_MAP_NAME), null, HERO_ROOM_OFFSET),
       (getTiledMap(BOSS_ROOM_MAP_NAME), BOSS_ROOM_MAP_NAME, BOSS_ROOM_OFFSET),
       (getTiledMap(TOP_KEY_ITEM_ROOM_NAME), TOP_KEY_ITEM_ROOM_NAME, TOP_KEY_ITEM_ROOM_OFFSET),
       (getTiledMap(BOTTOM_KEY_ITEM_ROOM_NAME), BOTTOM_KEY_ITEM_ROOM_NAME, BOTTOM_KEY_ITEM_ROOM_OFFSET),
-      (getTiledMap(innerRooms(0)), innerRooms(0), INNER_ROOM_MAP_OFFSET(0)),
-      (getTiledMap(innerRooms(1)), innerRooms(1), INNER_ROOM_MAP_OFFSET(1)),
-      (getTiledMap(innerRooms(2)), innerRooms(2), INNER_ROOM_MAP_OFFSET(2)),
-      (getTiledMap(innerRooms(3)), innerRooms(3), INNER_ROOM_MAP_OFFSET(3)),
-      (getTiledMap(innerRooms(4)), innerRooms(4), INNER_ROOM_MAP_OFFSET(4)),
-      (getTiledMap(innerRooms(5)), innerRooms(5), INNER_ROOM_MAP_OFFSET(5)),
+      (getTiledMap(innerRooms(0)), null, INNER_ROOM_MAP_OFFSET(0)),
+      (getTiledMap(innerRooms(1)), null, INNER_ROOM_MAP_OFFSET(1)),
+      (getTiledMap(innerRooms(2)), null, INNER_ROOM_MAP_OFFSET(2)),
+      (getTiledMap(innerRooms(3)), null, INNER_ROOM_MAP_OFFSET(3)),
+      (getTiledMap(innerRooms(4)), null, INNER_ROOM_MAP_OFFSET(4)),
+      (getTiledMap(innerRooms(5)), null, INNER_ROOM_MAP_OFFSET(5)),
       (getTiledMap(INNER_BORDER_NAMES(Random.nextInt(INNER_BORDER_NAMES.length))), null, INNER_BORDER_OFFSET))
   }
 
@@ -89,10 +89,10 @@ class TileMapHelper {
           case "ground" => spawnEntity(() => EntitiesFactoryImpl.createImmobileEntity(EntityType.Immobile, size, position, EntityCollisionBit.Immobile, EntityCollisionBit.Hero | EntityCollisionBit.Enemy | EntityCollisionBit.Arrow | EntityCollisionBit.EnemyAttack))
           case "bridge" => spawnEntity(() => EntitiesFactoryImpl.createPlatform(position, size))
           case "door" =>
-            if(mapName.equalsIgnoreCase(BOSS_ROOM_MAP_NAME)){} //TODO spawn boss room door
+            if(mapName!=null && mapName.equalsIgnoreCase(BOSS_ROOM_MAP_NAME)){} //TODO spawn boss room door
             else spawnEntity(() => EntitiesFactoryImpl.createDoor(size, position))
           case "chest" =>
-            if(mapName.equalsIgnoreCase(TOP_KEY_ITEM_ROOM_NAME) || mapName.equalsIgnoreCase(BOTTOM_KEY_ITEM_ROOM_NAME))
+            if(mapName!=null && (mapName.equalsIgnoreCase(TOP_KEY_ITEM_ROOM_NAME) || mapName.equalsIgnoreCase(BOTTOM_KEY_ITEM_ROOM_NAME)))
               if (mapName.contains(keyLocation)) {} //TODO spawnare la chiave per il boss
               else {} //TODO spawnare un oggetto speciale
             else spawnEntity(() => EntitiesFactoryImpl.createImmobileEntity(EntityType.Immobile, size, position, EntityCollisionBit.Immobile, EntityCollisionBit.Hero | EntityCollisionBit.Enemy | EntityCollisionBit.Arrow | EntityCollisionBit.EnemyAttack))
@@ -100,7 +100,7 @@ class TileMapHelper {
           case "water" => spawnEntity(() => EntitiesFactoryImpl.createWaterPool(position,size))
           case "lava" => spawnEntity(() => EntitiesFactoryImpl.createLavaPool(position, size))
           case "enemy" =>
-            if(mapName.equalsIgnoreCase(BOSS_ROOM_MAP_NAME)) spawnEntity(() => EntitiesFactoryImpl.createWizardBossEnemy(position))
+            if(mapName!=null && mapName.equalsIgnoreCase(BOSS_ROOM_MAP_NAME)) spawnEntity(() => EntitiesFactoryImpl.createWizardBossEnemy(position))
             else spawnEntity(() => EntitiesFactoryImpl.createEnemies(size, position))
           case "portal" => //TODO spawn portal to new world (inactive)
           case _ => println("not supported layer: " + layer.getName)
