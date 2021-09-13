1027316240ac796a36f3c0d07d46e32cef0ce6e1
Luca Bracchi <luca.bracchi3@studio.unibo.it>
2021-09-13 23:33:34 +0200

    added seed-based world generation

--- src/main/scala/view/screens/helpers/TileMapManager.scala
+++ src/main/scala/view/screens/helpers/TileMapManager.scala
@@ -1,7 +1,7 @@
 
 package view.screens.helpers
 
-import _root_.utils.ApplicationConstants.PIXELS_PER_METER
+import _root_.utils.ApplicationConstants.{PIXELS_PER_METER, RANDOM_SEED, ROOM_MAP_NAMES}
 import _root_.utils.MapConstants._
 import com.badlogic.gdx.maps.objects.RectangleMapObject
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer
@@ -19,38 +19,41 @@ class TileMapHelper {
   private val scale: Float = 1/(PIXELS_PER_METER/2)
   private var keyLocation: String = null
 
-  //array: TiledMap, mapName, mapOffset
-  private var tiledMapList: Array[(TiledMap, String, (Integer, Integer))] = Array()
+  //array: TiledMap, mapOffset, mapName
+  private var tiledMapList: Array[(TiledMap, (Integer, Integer), String)] = Array()
 
   def getMapRenderer(tiledMap: TiledMap): OrthogonalTiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap, scale)
 
   def getTiledMap(mapName: String): TiledMap = new TmxMapLoader().load("assets/maps/" + mapName + ".tmx")
 
+//  def loadTiledMaps(seed: Int): Unit = {
   def loadTiledMaps(): Unit = {
-    //scelgo casualmente 6 stanze da mettere nel world (le stanze non devono ripetersi)
-    var innerRooms: Array[String] = Array()
-    while (innerRooms.length < 6){
-      val room: String = INNER_ROOM_MAP_NAMES(Random.nextInt(INNER_ROOM_MAP_NAMES.size))
-      if(!innerRooms.contains(room)) innerRooms = innerRooms :+ room
-    }
 
+    if(RANDOM_SEED%2 == 0) keyLocation = "TOP"
+    else keyLocation = "BOTTOM"
+
+    //scelgo casualmente 6 stanze da mettere nel world (le stanze non devono ripetersi)
+    val innerRooms: Array[String] = getNonStaticRooms(RANDOM_SEED)
+
+    innerRooms.foreach(elem => println(elem))
     this.tiledMapList = Array(
-      (getTiledMap(WORLD_LEFT_BORDER_NAME), null, WORLD_LEFT_BORDER_OFFSET),
-      (getTiledMap(WORLD_TOP_BORDER_NAME), null, WORLD_TOP_BORDER_OFFSET),
-      (getTiledMap(WORLD_RIGHT_BORDER_NAME), null, WORLD_RIGHT_BORDER_OFFSET),
-      (getTiledMap(WORLD_BOTTOM_BORDER_NAME), null, WORLD_BOTTOM_BORDER_OFFSET),
-      (getTiledMap(HERO_ROOM_MAP_NAME), null, HERO_ROOM_OFFSET),
-      (getTiledMap(BOSS_ROOM_MAP_NAME), BOSS_ROOM_MAP_NAME, BOSS_ROOM_OFFSET),
-      (getTiledMap(innerRooms(0)), null, INNER_ROOM_MAP_OFFSET(0)),
-      (getTiledMap(innerRooms(1)), null, INNER_ROOM_MAP_OFFSET(1)),
-      (getTiledMap(innerRooms(2)), null, INNER_ROOM_MAP_OFFSET(2)),
-      (getTiledMap(innerRooms(3)), null, INNER_ROOM_MAP_OFFSET(3)),
-      (getTiledMap(innerRooms(4)), null, INNER_ROOM_MAP_OFFSET(4)),
-      (getTiledMap(innerRooms(5)), null, INNER_ROOM_MAP_OFFSET(5)),
-      (getTiledMap(INNER_BORDER_NAMES(Random.nextInt(INNER_BORDER_NAMES.length))), null, INNER_BORDER_OFFSET),
-      (getTiledMap(TOP_KEY_ITEM_ROOM_NAME), TOP_KEY_ITEM_ROOM_NAME, TOP_KEY_ITEM_ROOM_OFFSET),
-      (getTiledMap(BOTTOM_KEY_ITEM_ROOM_NAME), BOTTOM_KEY_ITEM_ROOM_NAME, BOTTOM_KEY_ITEM_ROOM_OFFSET),
+      (getTiledMap(WORLD_LEFT_BORDER_NAME), WORLD_LEFT_BORDER_OFFSET, null),
+      (getTiledMap(WORLD_TOP_BORDER_NAME), WORLD_TOP_BORDER_OFFSET, null),
+      (getTiledMap(WORLD_RIGHT_BORDER_NAME), WORLD_RIGHT_BORDER_OFFSET, null),
+      (getTiledMap(WORLD_BOTTOM_BORDER_NAME), WORLD_BOTTOM_BORDER_OFFSET, null),
+      (getTiledMap(HERO_ROOM_MAP_NAME), HERO_ROOM_OFFSET, null),
+      (getTiledMap(BOSS_ROOM_MAP_NAME), BOSS_ROOM_OFFSET, BOSS_ROOM_MAP_NAME),
+      (getTiledMap(innerRooms(0)), INNER_ROOM_MAP_OFFSET(0), null),
+      (getTiledMap(innerRooms(1)), INNER_ROOM_MAP_OFFSET(1), null),
+      (getTiledMap(innerRooms(2)), INNER_ROOM_MAP_OFFSET(2), null),
+      (getTiledMap(innerRooms(3)), INNER_ROOM_MAP_OFFSET(3), null),
+      (getTiledMap(innerRooms(4)), INNER_ROOM_MAP_OFFSET(4), null),
+      (getTiledMap(innerRooms(5)), INNER_ROOM_MAP_OFFSET(5), null),
+      (getTiledMap(innerRooms(6)), INNER_BORDER_OFFSET, null),
+      (getTiledMap(TOP_KEY_ITEM_ROOM_NAME), TOP_KEY_ITEM_ROOM_OFFSET, TOP_KEY_ITEM_ROOM_NAME),
+      (getTiledMap(BOTTOM_KEY_ITEM_ROOM_NAME), BOTTOM_KEY_ITEM_ROOM_OFFSET, BOTTOM_KEY_ITEM_ROOM_NAME)
     )
+
   }
 
   def renderWorld(orthogonalTiledMapRenderer: OrthogonalTiledMapRenderer) : Unit = {
@@ -58,8 +61,8 @@ class TileMapHelper {
 
       //setto l'offset di renderizzazione
       tiledMap._1.getLayers.forEach(layer => {
-        layer.setOffsetX(tiledMap._3._1*8)
-        layer.setOffsetY(tiledMap._3._2*8)
+        layer.setOffsetX(tiledMap._2._1*8)
+        layer.setOffsetY(tiledMap._2._2*8)
       })
 
       orthogonalTiledMapRenderer.setMap(tiledMap._1)
@@ -68,14 +71,10 @@ class TileMapHelper {
   }
 
   def setWorld(): Unit = {
-
-    val possibleLocation : Array[String] = Array("TOP", "DOWN")
-    keyLocation = possibleLocation(Random.nextInt(possibleLocation.length))
-
     tiledMapList.foreach(room => loadRoomObjects(room._1, room._2, room._3))
   }
 
-  def loadRoomObjects(tiledMap: TiledMap, mapName:String, offset: (Integer, Integer)): Unit = {
+  def loadRoomObjects(tiledMap: TiledMap, offset: (Integer, Integer), mapName:String): Unit = {
     var rect: Rectangle = new Rectangle()
 
     tiledMap.getLayers.forEach(layer => {
@@ -96,7 +95,7 @@ class TileMapHelper {
             else spawnEntity(() => EntitiesFactoryImpl.createDoor(size, position))
           case "chest" =>
             if(mapName!=null && (mapName.equalsIgnoreCase(TOP_KEY_ITEM_ROOM_NAME) || mapName.equalsIgnoreCase(BOTTOM_KEY_ITEM_ROOM_NAME)))
-              if (mapName.contains(keyLocation))
+              if (mapName.toUpperCase.contains(keyLocation))
                 spawnEntity(() => EntitiesFactoryImpl.createItem(ItemPools.Keys, size, position))
               else
                 spawnEntity(() => EntitiesFactoryImpl.createItem(ItemPools.Default, size, position))
@@ -120,5 +119,27 @@ class TileMapHelper {
   }
 
   private def spawnEntity(f:() => Unit): Unit = EntitiesFactoryImpl.addPendingEntityCreation(f)
+
+  //in base al seed restituisce le stanze non fisse: le 6 stanze interne e il bordo interno del world
+  private def getNonStaticRooms(seed: Int): Array[String] = {
+    var rooms: Array[String] = Array()
+
+    var index = seed % INNER_ROOM_MAP_NAMES.length
+    //il secondo index serve per non rimanere nel loop per sempre
+    var supportIndex : Integer = 0
+
+    while(rooms.length < 6){
+      if(!rooms.contains(INNER_ROOM_MAP_NAMES(index))) rooms = rooms :+ INNER_ROOM_MAP_NAMES(index)
+      //aggiorno l'index per la prossima iterazione e il supportIndex per evitare scenari ciclici infiniti
+      index = (seed+supportIndex) % INNER_ROOM_MAP_NAMES.length
+      supportIndex = supportIndex+1
+    }
+
+    //genero un inner-border in base al seed
+    index = seed % INNER_BORDER_NAMES.length
+    rooms = rooms :+ INNER_BORDER_NAMES(index)
+
+    rooms
+  }
 
 }
